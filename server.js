68b9e3bbc7c61b0eeefbc2923d60ec66981894b7
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-01-13 15:10:02 +0100

    create file metadata API

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const fileUpload = require("express-fileupload");
+require("dotenv").config();
 
-var app = express();
+const app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use(fileUpload());
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-
-
+app.post("/api/fileanalyse", (req, res) => {
+  if (!req.files || Object.keys(req.files).length === 0)
+    return res.status(400).json({ error: "No file was uploaded" });
+  const { name, mimetype, size } = req.files.upfile;
+  res.json({ name: name, type: mimetype, size: size });
+});
 
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+app.listen(port, () => {
+  console.log("Your app is listening on port " + port);
 });
